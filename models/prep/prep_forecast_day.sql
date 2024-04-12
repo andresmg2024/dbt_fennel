d549ff47f5eca3345a5cd6c776e115dde7e9ce0d
andresmg2024 <morenogomez_1989@hotmail.com>
2024-04-12 16:56:40 +0200

    prep_forecast_day

--- models/prep/prep_forecast_day.sql
+++ models/prep/prep_forecast_day.sql
@@ -3,13 +3,17 @@ WITH forecast_day_data AS (
     FROM {{ref('staging_forecast_day')}}
 ),
 add_features AS (
-    SELECT *
-        ,...(...) AS day_of_month -- day of month as a number
-        ,...(...) AS month_of_year -- month name as a text
-        ,...(...) AS year -- year as a number
-        ,...(...) AS day_of_week -- weekday name as text
-        ,...(...) AS week_of_year -- calender week number as number
-        ,...(...) AS year_and_week -- year-calenderweek as text like '2024-43'
+ SELECT *
+        ,DATE_PART('day', date) AS day_of_month -- day of month as a number
+        ,TO_CHAR(date, 'Month') AS month_of_year -- month name as a text
+        ,DATE_PART('year', date) AS year -- year as a number
+        ,TO_CHAR(date, 'Day') AS day_of_week -- weekday name as text
+        ,DATE_PART('week', date) AS week_of_year -- calender week number as number
+        ,TO_CHAR(date, 'YYYY-IW') AS year_and_week -- year-calenderweek as text like '2024-43'
+    FROM forecast_day_data
+)
+SELECT *
+FROM add_features
 
     FROM forecast_day_data
 )
